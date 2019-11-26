75808467bab87c70b6c675f3f11f7e9212625eea
Nihal Patel <nihalmpatel@gmail.com>
2019-11-25 21:11:13 -0500

    Backend API Integration

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  predictStroke(body) {
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json');
+    return this.http.post(environment.apiURL + '/stroke/predictSentiment', body, { headers: headers });
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://173.193.82.130:32000'
 };
